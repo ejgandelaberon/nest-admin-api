cef85bfbd8c299f321f21e69f48fae019c986bd7
Emmanuel Joseph Beron <emsephron@gmail.com>
2022-07-09 13:19:29 +0800

    edit create, add search to findAll, add count fn

--- src/user-profile/user-profile.service.ts
+++ src/user-profile/user-profile.service.ts
@@ -1,8 +1,15 @@
 import { Model } from 'mongoose';
-import { Injectable, Inject } from '@nestjs/common';
+import {
+  Injectable,
+  Inject,
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { UserProfile } from './interfaces/user-profile.interface';
+import { UserProfileMessages } from './user-profile.enum';
 import { CreateUserProfileDto } from './dto/create-user-profile.dto';
 import { UpdateUserProfileDto } from './dto/update-user-profile.dto';
+import { Request } from 'express';
 
 @Injectable()
 export class UserProfileService {
@@ -12,15 +19,54 @@ export class UserProfileService {
   ) {}
 
   async create(dto: CreateUserProfileDto): Promise<UserProfile> {
-    const createdUserProfile = new this.userProfileModel({
-      ...dto,
-      createdAt: new Date(),
-    });
-    return createdUserProfile.save();
+    try {
+      const uid = await this.userProfileModel.findOne({ uid: dto.uid }, 'uid');
+      if (uid) throw new BadRequestException(UserProfileMessages.EXISTS);
+      const createdUserProfile = new this.userProfileModel({
+        ...dto,
+        createdAt: new Date(),
+      });
+      return createdUserProfile.save();
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
-  async findAll(): Promise<UserProfile[]> {
-    return this.userProfileModel.find().exec();
+  async findAll(req?: Request) {
+    let query = {};
+    let sortOption = {};
+
+    if (req.query.s) {
+      query = {
+        $or: [
+          { displayName: new RegExp(req.query.s.toString(), 'i') },
+          { photoUrl: new RegExp(req.query.s.toString(), 'i') },
+        ],
+      };
+    }
+
+    if (req.query.sort) {
+      sortOption = {
+        displayName: req.query.sort,
+      };
+    }
+
+    const page: number = parseInt(req.query.page as string) || 1;
+    const limit: number = parseInt(req.query.limit as string) || 10;
+    const total = await this.count(query);
+
+    const data = await this.userProfileModel
+      .find(query)
+      .sort(sortOption)
+      .skip((page - 1) * limit)
+      .limit(limit);
+
+    return {
+      data,
+      total,
+      page,
+      last_page: Math.ceil(total / limit),
+    };
   }
 
   async findById(id: string): Promise<UserProfile> {
@@ -29,7 +75,11 @@ export class UserProfileService {
 
   async update(id: string, dto: UpdateUserProfileDto): Promise<UserProfile> {
     await this.userProfileModel
-      .findByIdAndUpdate(id, { ...dto, modifiedAt: new Date() })
+      .findByIdAndUpdate(id, {
+        displayName: dto.displayName,
+        photoUrl: dto.photoUrl,
+        modifiedAt: new Date(),
+      })
       .exec();
     const updated = await this.findById(id);
     return updated;
@@ -37,5 +87,9 @@ export class UserProfileService {
 
   async remove(id: string): Promise<UserProfile> {
     return this.userProfileModel.findByIdAndDelete(id).exec();
+  }
+
+  async count(query: any) {
+    return this.userProfileModel.count(query);
   }
 }
