1483434466e43ca5a03c39a75d9e30c9db678ca0
Emmanuel Joseph Beron <emsephron@gmail.com>
2022-07-09 13:14:45 +0800

    add user-profile enum

---
+++ src/user-profile/user-profile.enum.ts
@@ -0,0 +1,7 @@
+export enum UserProfileMessages {
+  NOT_FOUND = 'User profile not found.',
+  EXISTS = 'User profile already exists.',
+  CREATED = 'User profile has been created.',
+  UPDATED = 'User profile has been updated.',
+  REMOVED = 'User profile has been deleted.',
+}
