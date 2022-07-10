428e2dd9b18d1fcf7ded82a4eaf873b15701dff4
Emmanuel Joseph Beron <emsephron@gmail.com>
2022-07-10 08:41:45 +0800

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   const port = process.env.PORT || 3333;
   await app.listen(port, () =>
     console.log(`Running server on http://localhost:${port}`),
