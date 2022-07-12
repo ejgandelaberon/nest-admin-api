22c3fc92559be81be35a32fa76d32cb17bfc7c6b
Emmanuel Joseph Beron <emsephron@gmail.com>
2022-07-12 12:39:29 +0800

    add cors as option

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
   const port = process.env.PORT || 3333;
   await app.listen(port, () =>
     console.log(`Running server on http://localhost:${port}`),
